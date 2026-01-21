0d9cf02b7d895dbb1d990ae4062472de56dc3c0d
muhammadrehan589 <muhammadrehann589@gmail.com>
2026-01-21 23:28:46 +0500

    Some UI fix and changes in java code

--- android/app/src/main/java/com/example/hldsn/IncidentAdapter.java
+++ android/app/src/main/java/com/example/hldsn/IncidentAdapter.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
